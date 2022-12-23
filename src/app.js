baa8b7d06b439b16a5c227e67e38021206712c8c
irvan taufik <irvantaufik28@gmail.com>
2022-12-23 21:46:30 +0700

    setup app

--- src/app.js
+++ src/app.js
@@ -1,24 +1,24 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
-const cors = require('cors');
-const serverError = require('./middlerware/serverError');
+const express = require("express");
+const cors = require("cors");
+const serverError = require("./middlerware/serverError");
 
+const EmployeeRepository = require("./repository/employee_repo");
+const EmployeeUseCase = require("./usecase/employee_usecase");
 
-
+const employeeUC = new EmployeeUseCase(new EmployeeRepository());
 
 const app = express();
 
 app.use((req, res, next) => {
-
+  req.employeeUC = employeeUC;
   next();
 });
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
-
 
 app.use(serverError);
 
