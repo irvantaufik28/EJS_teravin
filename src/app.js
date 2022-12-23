5d24e86f9747a102c593488a5817772e5b5b310b
irvan taufik <irvantaufik28@gmail.com>
2022-12-23 21:24:50 +0700

    setup project

---
+++ src/app.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+const serverError = require('./middlerware/serverError');
+
+
+
+
+const app = express();
+
+app.use((req, res, next) => {
+
+  next();
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+
+
+app.use(serverError);
+
+module.exports = app;
