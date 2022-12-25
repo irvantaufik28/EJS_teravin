35ece43589581ed6d4641dbc260032ce8975f43c
Irvan Taufik <107734134+irvantaufik28@users.noreply.github.com>
2022-12-25 22:25:51 +0700

    Delete src directory

--- src/middlerware/serverError.js
+++
@@ -1,9 +0,0 @@
-// eslint-disable-next-line no-unused-vars
-module.exports = (error, req, res, next) => {
-  // eslint-disable-next-line no-console
-  console.log(error);
-  res.status(500).json({
-    status: 'failed',
-    message: 'internal server error',
-  });
-};
--- src/routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-
-const rootRouter = express.Router();
-
-const employee = require('./employee_router');
-
-rootRouter.use('/', employee);
-module.exports = rootRouter;
