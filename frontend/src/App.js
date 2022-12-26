ce3b422e06fdcb0e054ae7c0415b1d718ebaf02a
irvan taufik <irvantaufik28@gmail.com>
2022-12-26 08:04:14 +0700

    FE fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,20 @@
+import { Container, Navbar } from "react-bootstrap"
+import HomePage from "./pages/home"
 
-import EmployeeList from "./components/EmployeeList";
 function App() {
   return (
-    <div>
-     <EmployeeList/>
-    </div>
-  );
+    <>
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">TERAVIN</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        </Container>
+      </Navbar>
+      <Container className="mt-5 p-5">
+        <HomePage />
+      </Container>
+    </>
+  )
 }
 
-export default App;
+export default App
